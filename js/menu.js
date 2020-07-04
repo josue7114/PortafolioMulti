5ddf52c7eb07bbcaac3d3f2e90d870b4be2aa25e
Josue <josue7114@gmail.com>
2020-07-04 17:44:02 -0600

    Menu responsive

---
+++ js/menu.js
@@ -0,0 +1,34 @@
+$(function(){
+    //header oculto
+    var header = document.getElementById('header');
+    var headroom = new Headroom(header);
+    headroom.init();
+
+    var ancho = $(window).width(),
+        links = $('#container__link'),
+        btnmenu = $('#btn-menu'), 
+        icono = $('#btn-menu .icono');
+
+        if(ancho < 600){
+            links.hide();
+            icono.addClass('fa-bars');
+        }
+
+        btnmenu.on('click', function(e){
+            links.slideToggle();
+            icono.toggleClass('fa-bars');
+            icono.toggleClass('fa-times');
+        });
+
+        $(window).on('resize', function(){
+            if($(this).width() > 600){
+                links.show();
+                icono.addClass('fa-times');
+                icono.removeClass('fa-bars');
+            }else{
+                links.hide();
+                icono.addClass('fa-bars');
+                icono.removeClass('fa-times');
+            }
+        });
+});
